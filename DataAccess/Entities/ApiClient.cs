8852681af46926a007aca5b76976c5d90087985e
Boštjan Klemenc <bostjan.klemenc@ema.si>
2026-05-31 20:54:27 +0200

    feat: added apiClient annotations

--- DataAccess/Entities/ApiClient.cs
+++ DataAccess/Entities/ApiClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace DataAccess.Entities
     {
         public int Id { get; set; }
 
+        [StringLength(50)]
         public string ClientName { get; set; }
 
+        [StringLength(50)]
         public string ApiKey { get; set; }
 
         public bool IsActive { get; set; } = true;
---
+++ DataAccess/Migrations/20260531185336_ApiClientAnnotations.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiClientAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientName",
+                table: "ApiClients",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiKey",
+                table: "ApiClients",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "ApiClients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 31, 18, 53, 35, 934, DateTimeKind.Utc).AddTicks(1604));
+
+            migrationBuilder.UpdateData(
+                table: "ApiClients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 31, 18, 53, 35, 934, DateTimeKind.Utc).AddTicks(1606));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientName",
+                table: "ApiClients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiKey",
+                table: "ApiClients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "ApiClients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 31, 18, 39, 8, 335, DateTimeKind.Utc).AddTicks(3978));
+
+            migrationBuilder.UpdateData(
+                table: "ApiClients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 31, 18, 39, 8, 335, DateTimeKind.Utc).AddTicks(3980));
+        }
+    }
+}
